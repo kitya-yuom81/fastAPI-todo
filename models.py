4d814919af12cb7d76b3c4a49e379447773ba48b
Yuom Kitya <kityayuom@gmail.com>
2025-09-17 15:17:16 +0700

    update the model

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
+from typing import Optional
+from pydantic import BaseModel, Field
 
-from pydantic import BaseModel
-
-class Todo(BaseModel):
-    id: int | None = None
-    title: str
+class TodoIn(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
     completed: bool = False
+
+class TodoUpdate(BaseModel):
+    title: Optional[str] = Field(default=None, min_length=1, max_length=200)
+    completed: Optional[bool] = None
+
+class TodoOut(TodoIn):
+    id: int
